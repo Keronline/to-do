f5e0a3dbc3d4273b64826902b14565abbe159e9b
Karoline Borges <tecnologia.karol@gmail.com>
2022-04-28 09:22:38 -0300

    Aula 3

---
+++ assets/js/todo.js
@@ -0,0 +1,16 @@
+console.log("Hello, World");
+
+const nome = 'Karol';
+let email = 'tecnologia.karol@gmail.com';
+console.log(`Nome: ${nome}  Email: ${email}`);
+
+if((nome == "Karol")){
+    console.log('Nome correto');
+}
+
+function escreve(x){
+    console.log("Frase:" + x);
+}
+
+escreve(`Seja bem vinda, ${nome}`);
+escreve("Treinamento HTML5, CSS3 e JS")
